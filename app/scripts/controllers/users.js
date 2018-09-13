15e711c0b30ffad8ee32170e84c71283e1a2aacb
SedielRuiz <SedielRuiz21@gmail.com>
2018-09-12 21:59:11 -0500

    add users

--- app/scripts/controllers/users.js
+++ app/scripts/controllers/users.js
@@ -3,6 +3,26 @@
  * usuarios
  */
 
+var users=[];
 function usersActionsController() {
-    //tu código aquí.  
+    
+}
+function saveUser(){
+	var s = confirm("¿Seguro que desa guardar el usuario: "+$("#name").val()+"?");
+	if(s){
+		users.push({
+			"name": $("#name").val(),
+			"lastName": $("#lastName").val(),
+			"document": $("#document").val(),
+			"phone": $("#phone").val()
+		});
+		clear();
+		console.log(users);
+	}else{return 0;}
+}
+function clear(){
+	$("#name").val("");
+	$("#lastName").val("");
+	$("#document").val("");
+	$("#phone").val("");
 }
--- app/scripts/controllers/versioner.js
+++ app/scripts/controllers/versioner.js
@@ -3,3 +3,6 @@
  */
 function versionerActionsController() {
 };
+function saveVersion(){
+
+}
